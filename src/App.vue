93ff405037a16f06da3c49d40577072ee7dd2978
jibranjs <jibranshaheer2@gmail.com>
2025-11-12 02:04:45 +0500

    Assignment

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import BasicButton from './components/BasicComponent/BasicButton.vue';
+const headingId = "color";
+const okay = false;
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1 :class="headingId"> {{ okay ? 'Yes' : 'No' }}</h1>
+      <BasicButton>Testing</BasicButton>
+    </div>
+  </header>
+
+</template>
+
+<style>
+.color{
+  color: #0F8FFF;
+}
+</style>
+
---
+++ src/components/BasicComponent/BasicButton.vue
@@ -0,0 +1,9 @@
+<template>
+<Button class="!bg-[#ddd]">
+    <slot></slot>
+</Button>
+    </template>
+
+    <script setup>
+import { Button } from 'primevue';
+</script>
