5bb65478151e5bb8b777d7220ff86eb0860500de
jibranjs <jibranshaheer2@gmail.com>
2025-11-14 08:49:05 -0500

    Journey Completed

--- src/Views/HomeView.vue
+++ src/Views/HomeView.vue
@@ -1,8 +1,32 @@
-<script setup lang="ts">
-import ProductCard from '@/components/ProductCard.vue';
+<script setup>
+import Button from 'primevue/button';
+import Stepper from 'primevue/stepper';
+import StepList from 'primevue/steplist';
+import StepPanels from 'primevue/steppanels';
+import StepItem from 'primevue/stepitem';
+import Step from 'primevue/step';
+import StepPanel from 'primevue/steppanel';
+import InputText from 'primevue/inputtext';
+import { ref } from 'vue';
+import JourneyComponent from '@/components/journeyComponent.vue';
 
+const value = ref()
 </script>
-
 <template>
-    <ProductCard />
+    <div class="card flex justify-center">
+        <Stepper value="1" class="basis-[50rem]">
+            <StepList>
+                <Step value="1">Journey</Step>
+                <Step value="2">Vechile</Step>
+                <Step value="3">Info</Step>
+                <Step value="4">Pay</Step>
+            </StepList>
+            <StepPanels>
+
+                <JourneyComponent />
+        
+
+            </StepPanels>
+        </Stepper>
+    </div>
 </template>
---
+++ src/components/journeyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <StepPanel v-slot="{ activateCallback }" value="1">
+    <form sb class="flex flex-col py-12">
+       <label class="font-bold !mx-[50px] !mb-1">PICKUP LOCATION</label>
+            <InputText type="Text" v-model="value" placeholder="Pick Up Location" class="!bg-white !text-black !mb-4 !mx-12" />
+       <label class="font-bold !mx-[50px] !mb-1">DROP OFF LOCATION</label>
+            <InputText type="Text" required="" placeholder="Pick Up Location" class="!bg-white !text-black !mx-12" />
+        <div class="!mx-12 !mt-3 flex justify-end">
+            <Button label="Next" @click="activateCallback('2')" severity="info" class="!text-white !px-6 !py-2 !mb-3" />
+        </div>
+    </form>
+    </StepPanel>
+</template>
+<script setup>
+
+import StepPanel from 'primevue/steppanel';
+import InputText  from 'primevue/inputtext';
+import Button from 'primevue/button';
+</script>
